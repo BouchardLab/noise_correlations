0cda8d901f5b131662fcbbd94c4528f5c0083885
Jesse Livezey <jesse.livezey@gmail.com>
2019-10-14 12:55:21 -0400

    updates

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,7 +1,7 @@
 export MKL_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
-for dim in {2..10}; do
+for dim in {4..10}; do
   echo $dim
-  mpirun -n $1 python -u dist_analysis.py /data/noise_correlations . $2 $dim
+  mpirun -n $1 python -u dist_analysis.py /data/noise_correlations . $2 $dim -n $3
 done
